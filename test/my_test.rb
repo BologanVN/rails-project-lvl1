01dedc44572194e11c43dd0ad709dafac07f5006
Valeria Bologan <valeria.bologan@insales.ru>
2022-04-24 14:13:16 +0300

    step3

--- test/my_test.rb
+++ test/my_test.rb
@@ -1,16 +1,15 @@
-#frozen_string_literal: true
+# frozen_string_literal: true
 
 require 'minitest/autorun'
 require 'hexlet_code'
 
 class MyTest < Minitest::Test
-
   def setup; end
 
   def tesrdown; end
 
   def test_br
-    expect_br='<br>'
-    assert( HexletCode::Tag.build('br') == expect_br )
+    expect_br = '<br>'
+    assert(HexletCode::Tag.build('br') == expect_br)
   end
 end
